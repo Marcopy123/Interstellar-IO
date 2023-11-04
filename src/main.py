bd365d482ba0cbfca02340d67dd1a9d6cb695c78
Tony <letuanhuynguyen@gmail..com>
2023-11-04 00:25:01 -0400

    pygame show particles

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ import random
 
 DT = 1 # Delta time
 
-WINDOW_WIDTH = 1200
-WINDOW_HEIGHT = 800
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
 NUM_OF_PARTICLES = 100
 
 
@@ -16,7 +16,7 @@ def draw(bodies, screen : pg.Surface):
     size = 50
     for i in bodies:
         body_rect = pg.Surface((size, size))
-        screen.blit(i, (i.pos[0] - size/2, i.pos[1] - size/2))
+        screen.blit(body_rect, (i.pos[0] - size/2, i.pos[1] - size/2))
 
 
 def main():
@@ -30,22 +30,24 @@ def main():
 
     screen = pg.display.set_mode(window_size)
 
+
     bodies = []
-    for i in range(NUM_OF_PARTICLES):
+    for i in range(2):
         xPos = random.randint(0, WINDOW_WIDTH)
         yPos = random.randint(0, WINDOW_HEIGHT)
-        bodies.append(Body(float(i), np.array([float(xPos), float(yPos)]), np.array([0.0, 0.0])))
+        bodies.append(Body(float(i + 1), np.array([float(xPos), float(yPos)]), np.array([0.0, 0.0])))
 
     running = True
     # pygame main loop
     while running:
+        screen.fill((255, 255, 255))
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
             
         for i in bodies:
             i.update(DT, bodies)
-
+        draw(bodies, screen)
         pg.display.flip()
     
     pg.quit()
