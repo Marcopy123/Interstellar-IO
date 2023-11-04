3778f2983868f7e21dd79e71dcb7d9973eb9262f
Justin Bax <justin.bax@icloud.com>
2023-11-03 22:39:43 -0400

    started main script

---
+++ src/main.py
@@ -0,0 +1,41 @@
+import math
+import numpy as np
+import pygame as pg
+
+BIG_G = 1 # Gravitational constant
+
+class Body:
+
+    def __init__(self, mass, pos, vel):
+        self.mass
+        self.pos = pos
+        self.vel = vel
+        return
+
+
+def gravitational_force(first, second):
+    distance_squared = sum(x**2 for x in (second.pos - first.pos))
+    return BIG_G * first.mass * second.mass / distance_squared
+
+
+def main():
+    print("interstellarIO")
+
+    bodies = []
+    for i in range(5):
+        bodies.append(Body(i, [i, i], [i, i])) # Random stuff
+
+    for i in bodies:
+        net_force = np.array([0, 0])
+
+        for j in bodies:
+            # TODO do probably need to check if i == j otherwise there will be a division by zero
+            # TODO optimize (like reuse the result of (i, j) for (j, i))
+            force = gravitational_force(i, j)
+
+            # TODO vectorize this scalar and add to net_force
+            # TODO maybe reuse calculated second.pos - first.pos with gravitational_force
+
+
+if __name__ == "__main__":
+    main()
