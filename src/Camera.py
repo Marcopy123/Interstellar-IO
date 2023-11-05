7dffeeafa49d7ec05b5d4055593194315aacf652
Justin Bax <justin.bax@icloud.com>
2023-11-05 10:06:48 -0500

    improved rendering of player

--- src/Camera.py
+++ src/Camera.py
@@ -61,9 +61,11 @@ class Camera:
         return pg.Color(color[0], color[1], color[2], a=0.5)
 
 
-    def draw(self, objs):
+    def draw(self, objs, camera):
         for i in objs:
             # Draw trail
+            if i.uid == camera.uid:
+                continue
             trail_idx = 0
             for j in i.trail:
                 x_pos = (j[0] - self.offset[0] - self.screen.get_size()[0] / 2) * self.zoom + self.screen.get_size()[0] / 2
@@ -77,3 +79,18 @@ class Camera:
             y_pos = (i.pos[1] - self.offset[1] - self.screen.get_size()[1] / 2) * self.zoom + self.screen.get_size()[1] / 2
             img = pg.transform.scale(i.image, (i.radius * self.zoom * 3, i.radius * self.zoom * 3))
             self.screen.blit(img, (x_pos - i.radius * self.zoom * 3/2 , y_pos - i.radius * self.zoom * 3/2))
+
+        # Do camera
+        trail_idx = 0
+        for j in camera.trail:
+            x_pos = (j[0] - self.offset[0] - self.screen.get_size()[0] / 2) * self.zoom + self.screen.get_size()[0] / 2
+            y_pos = (j[1] - self.offset[1] - self.screen.get_size()[1] / 2) * self.zoom + self.screen.get_size()[1] / 2
+            pg.draw.circle(self.screen, pg.Color(128, 128, 128, a=0.5), (x_pos, y_pos), camera.radius / 3 * self.zoom * (trail_idx + 1) / self.obj.max_trail)
+            #pg.draw.circle(self.screen, self.color_from_speed(j[2]), (x_pos, y_pos), i.radius/3 * self.zoom * (trail_idx + 1)/self.obj.max_trail)
+            trail_idx += 1
+        
+        # Draw object
+        x_pos = (camera.pos[0] - self.offset[0] - self.screen.get_size()[0] / 2) * self.zoom + self.screen.get_size()[0] / 2
+        y_pos = (camera.pos[1] - self.offset[1] - self.screen.get_size()[1] / 2) * self.zoom + self.screen.get_size()[1] / 2
+        img = pg.transform.scale(camera.image, (camera.radius * self.zoom * 3, camera.radius * self.zoom * 3))
+        self.screen.blit(img, (x_pos - camera.radius * self.zoom * 3/2 , y_pos - camera.radius * self.zoom * 3/2))
